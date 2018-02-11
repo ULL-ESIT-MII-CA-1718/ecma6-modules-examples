fb56c059da927fca4cdf5401e673b544d91b9bb1
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2018-02-11 10:36:23 +0000

    nodejs ecma6 modules

--- gulpfile.js
+++ gulpfile.js
@@ -5,3 +5,6 @@ gulp.task("serve", shell.task("static-server -p 8180"));
 
 // canary chrome > 60
 gulp.task("canary", shell.task("open http://localhost:8180/hello/index.html"));
+
+// Starting with version 8.5.0, Node.js supports ES modules natively, behind a command line option
+gulp.task("nodejs", shell.task("node --experimental-modules nodejs/esm-demo/main.mjs"));
---
+++ nodejs/README.md
@@ -0,0 +1,1 @@
+[Using ES6 modules natively in Node.js](http://2ality.com/2017/09/native-esm-node.html)
